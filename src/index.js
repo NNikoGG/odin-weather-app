ae6a7205acf6469ef9af01a88763554b401d293b
NNikoGG <geekgaming222@gmail.com>
2024-03-29 16:26:08 +0530

    Add function to return weather data

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./style.css";
 
-let helloWorld = document.createElement("div");
-helloWorld.innerText = "Hello World!";
-helloWorld.classList.add("hello");
+const apiKey = "93486148db0f4cbb96b100525242903";
+let query = "London";
 
-document.body.appendChild(helloWorld);
+async function getWeather() {
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${query}&aqi=no`);
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+getWeather();

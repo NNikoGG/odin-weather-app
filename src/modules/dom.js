5d7fe78c1850b693cac22e412d182d37ccf812e1
NNikoGG <geekgaming222@gmail.com>
2024-03-31 22:41:36 +0530

    Add display info functionality

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -3,13 +3,27 @@ import { getWeather } from "./api";
 function render() {
   const weatherForm = document.querySelector("#weather-form");
   const locationInput = document.querySelector("#location-input");
+  const locationName = document.querySelector(".location-name");
+  const localTime = document.querySelector(".local-time");
+  const temp = document.querySelector(".temp");
+  const feelsLikeTemp = document.querySelector(".feels-like-temp");
+  const humidity = document.querySelector(".humidity");
+  const windSpeed = document.querySelector(".wind-speed");
+  const windDirection = document.querySelector(".wind-direction");
+
   weatherForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     const location = locationInput.value;
 
     try {
       const data = await getWeather(location);
-      console.log(data);
+      locationName.textContent = `${data.location.name}, ${data.location.country}`;
+      localTime.textContent = `Local Time: ${data.location.localtime}`;
+      temp.textContent = `Temperature: ${data.current.temp_c} °C`;
+      feelsLikeTemp.textContent = `Feels Like: ${data.current.feelslike_c} °C`;
+      humidity.textContent = `Humidity: ${data.current.humidity}%`;
+      windSpeed.textContent = `Wind Speed: ${data.current.wind_kph} kph`;
+      windDirection.textContent = `Wind Direction: ${data.current.wind_dir}`;
     } catch (error) {
       console.error("Error fetching weather data:", error);
     }
